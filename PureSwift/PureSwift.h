879535ae29f142b478a1f9c68c54c40dfd895123
Artem Mylnikov (ajjnix) <ajjnix@gmail.com>
2017-04-20 23:04:36 +0700

    Make empty project

---
+++ PureSwift/PureSwift.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PureSwift.
+FOUNDATION_EXPORT double PureSwiftVersionNumber;
+
+//! Project version string for PureSwift.
+FOUNDATION_EXPORT const unsigned char PureSwiftVersionString[];
---
+++ PureSwiftTests/PureSwiftTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import PureSwift
+
+final class PureSwiftTests: XCTestCase {
+    func test_first() {
+        XCTFail("")
+    }
+}
